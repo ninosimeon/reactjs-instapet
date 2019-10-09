38623e821cb3a4eeb4306f3fd28c45caca59b8b4
Nino Simeon <ninosimeon@gmail.com>
2019-10-08 22:32:22 -0500

    integrated context

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Router } from '@reach/router'
-
-const UserLogged = ({ children }) => {
-  return children({ isAuth: false })
-}
+import Context from './Context'
 
 export const App = () => {
   return (
@@ -24,7 +21,7 @@ export const App = () => {
         <Detail path='/detail/:detailId'/>
       </Router>
 
-      <UserLogged>
+      <Context.Consumer>
         {
           ({ isAuth }) =>
             isAuth
@@ -37,7 +34,7 @@ export const App = () => {
                 <NotRegisteredUser path='/user'/>
               </Router>
         }
-      </UserLogged>
+      </Context.Consumer>
       <NavBar/>
     </div>
   )
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import Context from '../Context'
 
 export const NotRegisteredUser = () => (
-  <h1>Not Registered User!</h1>
+  <Context.Consumer>
+    {
+      ({ activateAuth }) => {
+        return (
+          <form onSubmit={activateAuth}>
+            <button>Iniciar Sesión</button>
+          </form>
+        )
+      }
+    }
+  </Context.Consumer>
 )
