84e2a163bd3a25f45c2dbc9fdb48d281cd55b7b5
Nino Simeon <nsimeon@lmsonline.com>
2019-10-09 10:25:07 -0500

    Register mutation with GraphQL

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
+import { RegisterMutation } from '../container/RegisterMutation'
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
     {
       ({ activateAuth }) => {
         return <>
-          <UserForm onSubmit={activateAuth} title='Registrarse'/>
+          <RegisterMutation>
+            {
+              (register) => {
+                const onSubmit = ({email, password}) => {
+                  const input = {email, password}
+                  const variables = {input}
+                  register({variables})
+                }
+
+                return <UserForm onSubmit={onSubmit} title='Registrarse'/>
+              }
+            }
+          </RegisterMutation>
           <UserForm onSubmit={activateAuth} title='Iniciar Sesión'/>
         </>
       }
