547ec53d55c35be8e56d01ccfc8344b4c1fa9e9a
Nino Simeon <ninosimeon@gmail.com>
2019-10-10 15:10:19 -0500

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { useContext } from 'react'
+import React, { Suspense, useContext } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 import { Home } from './pages/Home'
-import { Detail } from './pages/Detail'
-import { Favs } from './pages/Favs'
-import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Redirect, Router } from '@reach/router'
 import { Context } from './Context'
 import { NotFound } from './pages/NotFound'
 
+const Favs = React.lazy(() => import('./pages/Favs'));
+const User = React.lazy(() => import('./pages/User'));
+const Detail = React.lazy(() => import('./pages/Detail'));
+
 export const App = () => {
-  const { isAuth } = useContext(Context)
+  const {isAuth} = useContext(Context);
   return (
-    <div>
+    <Suspense fallback={<div/>}>
       <GlobalStyle/>
       <Logo/>
       <Router>
@@ -23,13 +24,13 @@ export const App = () => {
         <Home path='/pet/:categoryId'/>
         <Detail path='/detail/:detailId'/>
         {!isAuth && <NotRegisteredUser path='/login'/>}
-        {!isAuth && <Redirect from='/favs' to='/login'/>}
-        {!isAuth && <Redirect from='/user' to='/login'/>}
+        {!isAuth && <Redirect from='/favs' to='/login' noThrow/>}
+        {!isAuth && <Redirect from='/user' to='/login' noThrow/>}
         {isAuth && <Redirect from='/login' to='/'/>}
         <Favs path='/favs'/>
         <User path='/user'/>
       </Router>
       <NavBar/>
-    </div>
+    </Suspense>
   )
-}
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
 import { Layout } from '../components/Layout'
 
-export const Home = ({ categoryId }) => {
+const HomePage = ({categoryId}) => {
   return (
     <Layout
       title='Tu app de fotos de mascotas'
@@ -13,4 +13,8 @@ export const Home = ({ categoryId }) => {
       <ListOfPhotoCards categoryId={categoryId}/>
     </Layout>
   )
-}
+};
+
+export const Home = React.memo(HomePage, (prevProps, props) => {
+  return prevProps.categoryId === props.categoryId
+});
